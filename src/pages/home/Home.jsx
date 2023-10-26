51d5b9783c6794377fdc92d521addd6942a85fc9
NF RIDOY <nfridoy@gmail.com>
2023-10-27 00:51:51 +0600

    Feat: AuthProvider Tower Setup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import Services from "../../components/Services/Services";
+import { useContext } from "react";
 // import Footer from "../../components/Footer/Footer";
-
+import {AuthContext} from '../../Provider/AuthProvider'
 
 export default function Home() {
+  
+  const { test } = useContext(AuthContext)
 
   const servicesData = useLoaderData()
   console.log(servicesData)
   return (
     <div>
-      
+      <h1>{test}</h1>
       <Services servicesData={servicesData}></Services>
       {/* <Footer servicesData={servicesData}></Footer> */}
       
